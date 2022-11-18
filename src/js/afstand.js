cc9997b32c58c082b99491eaa5ebd0ba4eb0d0a8
Cyril Cornez <cyrilcornez@outlook.com>
2022-11-18 23:33:48 +0100

    Update starterkit

--- src/js/afstand.js
+++
@@ -1,1 +0,0 @@
-export default (x1, x2, y1, y2) => Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,5 @@
 import '../styles/style.scss';
-import calculateDistance from './afstand';
 import { randomIntFromInterval as random } from './helpers.js';
 
 console.log('test');
-console.log(
-  calculateDistance(random(0, 10), random(0, 10), random(0, 10), random(0, 10))
-);
+console.log(random(0, 10), random(0, 100));
