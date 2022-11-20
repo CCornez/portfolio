5a02e8b11eb02f965821dcc52441ab5fbdd037b0
Cyril Cornez <cyrilcornez@outlook.com>
2022-11-20 21:49:18 +0100

    Add env in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: { appDir: true },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 module.exports = nextConfig;
